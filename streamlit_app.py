2834f16af9ae5bbbf0cddf653c199577a2208eb6
spieczykolan <122131646+spieczykolan@users.noreply.github.com>
2024-08-16 10:50:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
     #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+   # my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+   #         values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
